9d8b8b32d9643e304bc6f3d2c40d263a10cfb8fe
Java <indead1124@gmail.com>
2023-03-23 20:48:26 -0400

    organize

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# userstyles
+<h1 align="center">userscripts // userstyles</p>
+
+<h2>scripts</h2>
+
+<h2>styles</h2>
--- scripts/desmos-no-homepage.js
+++ scripts/desmos-no-homepage.js
@@ -1,11 +1,8 @@
 // ==UserScript==
 // @name        desmos.com == desmos.com/calculator
-// @namespace   Violentmonkey Scripts
 // @match       https://www.desmos.com/
-// @grant       none
 // @version     1.0
 // @author      Java
-// @description 3/15/2023, 9:35:32 PM
 // ==/UserScript==
 
 window.open('/calculator', '_self')
